7a47b226bd2c6d2902ccc9b071a95c3c97b437b4
Vadim Butenko <vbutenko@Vadims-MacBook-Pro-2.local>
2020-05-11 14:36:50 -0700

    added sign in screen

---
+++ app/src/main/java/com/example/projemanag/IntroActivity.kt
@@ -0,0 +1,19 @@
+package com.example.projemanag
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+    }
+
+
+}
--- app/src/main/java/com/example/projemanag/SplashActivity.kt
+++ app/src/main/java/com/example/projemanag/SplashActivity.kt
@@ -1,8 +1,10 @@
 package com.example.projemanag
 
+import android.content.Intent
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.WindowManager
 import kotlinx.android.synthetic.main.activity_splash.*
 
@@ -18,5 +20,10 @@ class SplashActivity : AppCompatActivity() {
 
         val typeface: Typeface = Typeface.createFromAsset(assets, "carbon bl.ttf")
         tv_app_name.typeface = typeface
+
+        Handler().postDelayed({
+            startActivity(Intent(this, IntroActivity::class.java))
+            finish()
+        },2500)
     }
 }
