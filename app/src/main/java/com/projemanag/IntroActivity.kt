32dc2ff9433b9e654948b992604bc92ac09d1a83
Vadim Butenko <vbutenko@Vadims-MacBook-Pro-2.local>
2020-05-12 08:13:43 -0700

    changes

--- app/src/main/java/com/projemanag/IntroActivity.kt
+++ app/src/main/java/com/projemanag/IntroActivity.kt
@@ -23,7 +23,11 @@ class IntroActivity : AppCompatActivity() {
         val typeface: Typeface =
             Typeface.createFromAsset(assets, "carbon bl.ttf")
         tv_app_name_intro.typeface = typeface
+        btn_sign_in_intro.setOnClickListener {
 
+            // Launch the sign up screen.
+            startActivity(Intent(this@IntroActivity, SignInActivity::class.java))
+        }
         btn_sign_up_intro.setOnClickListener {
 
             // Launch the sign up screen.
