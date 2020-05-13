31a6c62ce1042327cc48971b94184b2cc3416702
Vadim Butenko <vbutenko@Vadims-MacBook-Pro-2.local>
2020-05-13 07:55:41 -0700

    added autoLogin

--- app/src/main/java/com/projemanag/activities/SplashActivity.kt
+++ app/src/main/java/com/projemanag/activities/SplashActivity.kt
@@ -7,16 +7,14 @@ import android.os.Handler
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.projemanag.R
+import com.projemanag.firebase.FirestoreClass
 import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        //This call the parent constructor
         super.onCreate(savedInstanceState)
-        // This is used to align the xml view to this class
         setContentView(R.layout.activity_splash)
 
-        // This is used to hide the status bar and make the splash screen as a full screen activity.
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
@@ -27,11 +25,16 @@ class SplashActivity : AppCompatActivity() {
             Typeface.createFromAsset(assets, "carbon bl.ttf")
         tv_app_name.typeface = typeface
 
-        // Adding the handler to after the a task after some delay.
         Handler().postDelayed({
-            // Start the Intro Activity
-            startActivity(Intent(this@SplashActivity, IntroActivity::class.java))
-            finish() // Call this when your activity is done and should be closed.
-        }, 2500) // Here we pass the delay time in milliSeconds after which the splash activity will disappear.
+
+            var currentUserID = FirestoreClass().getCurrentUserId()
+
+            if(currentUserID.isNotEmpty()){
+                startActivity(Intent(this, MainActivity::class.java))
+            }else{
+                startActivity(Intent(this, IntroActivity::class.java))
+            }
+            finish()
+        }, 2500)
     }
 }
--- app/src/main/java/com/projemanag/firebase/FirestoreClass.kt
+++ app/src/main/java/com/projemanag/firebase/FirestoreClass.kt
@@ -38,6 +38,11 @@ class FirestoreClass {
     }
 
     fun getCurrentUserId(): String{
-        return FirebaseAuth.getInstance().currentUser!!.uid
+        var currentUser = FirebaseAuth.getInstance().currentUser
+        var currentUserID = ""
+        if(currentUser != null) {
+            currentUserID = currentUser.uid
+        }
+        return currentUserID
     }
 }
