11b00a4d6b286d6f061dabdd34a077439ba8c3d4
Vadim Butenko <vbutenko@Vadims-MacBook-Pro-2.local>
2020-05-18 20:55:54 -0700

    added members layout

--- app/src/main/java/com/projemanag/activities/MembersActivity.kt
+++ app/src/main/java/com/projemanag/activities/MembersActivity.kt
@@ -2,11 +2,34 @@ package com.projemanag.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.telecom.Call
 import com.projemanag.R
+import com.projemanag.model.Board
+import com.projemanag.utils.Constants
+import kotlinx.android.synthetic.main.activity_members.*
+import kotlinx.android.synthetic.main.activity_my_profile.*
 
 class MembersActivity : AppCompatActivity() {
+
+    private lateinit var mBoardDetails: Board
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_members)
+        if(intent.hasExtra(Constants.BOARD_DETAIL)) {
+            mBoardDetails = intent.getParcelableExtra<Board>(Constants.BOARD_DETAIL)
+        }
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(toolbar_members_activity)
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+            actionBar.title = resources.getString(R.string.members)
+        }
+        toolbar_members_activity.setNavigationOnClickListener { onBackPressed() }
     }
 }
