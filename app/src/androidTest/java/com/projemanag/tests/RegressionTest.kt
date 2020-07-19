c0188002d74b52d650f6386d596ffe6b882f98e1
Vadim Butenko <vbutenko@Vadims-MBP-2.attlocal.net>
2020-07-19 12:21:36 -0700

    added comment

--- app/src/androidTest/java/com/projemanag/tests/RegressionTest.kt
+++ app/src/androidTest/java/com/projemanag/tests/RegressionTest.kt
@@ -48,7 +48,6 @@ class RegressionTest {
         private val updatedName = "Green"
         private val emptyCardText = "Add Card"
 
-        // adding comment
         @Test
         fun updateCardNameTest() {
             login {
